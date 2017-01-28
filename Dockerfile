29ba06ed59bdc125efbdec7cef7e2cae9bac474b
Oliver Stadie <hello@waog.net>
2017-01-28 01:32:39 +0100

    ADD default admin, script prints output, mongo is configured

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,11 @@ FROM mongo:3
 # Create script directory
 RUN mkdir -p /mongoAuthFromEnv
 COPY . /mongoAuthFromEnv
+RUN chmod +x /mongoAuthFromEnv/configureMongo.sh
 
-RUN chmod +x /mongoAuthFromEnv/insertEnvsIntoMongoScripts.sh
+# init default admin
+ENV MONGO_AUTH_ADMIN_NAME admin
+ENV MONGO_AUTH_ADMIN_PW 123456
+
+CMD ['/mongoAuthFromEnv/configureMongo.sh']
 
---
+++ configureMongo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o verbose
+
+# configure createAdmin.js
+sed -i "s/ADMIN_NAME_PLACEHOLDER/$MONGO_AUTH_ADMIN_NAME/g" /mongoAuthFromEnv/createAdmin.js
+sed -i "s/ADMIN_PW_PLACEHOLDER/$MONGO_AUTH_ADMIN_PW/g" /mongoAuthFromEnv/createAdmin.js
+
+# create the admin in the database
+mongo mongod/admin createAdmin.js
+
+# TODO: configure users
+# TODO: create the users in the database
+#mongo -u "admin" -p "123456" --authenticationDatabase "admin" mongod/test createUser.js
